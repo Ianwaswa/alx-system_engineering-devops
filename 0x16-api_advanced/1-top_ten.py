f2a31ceb99b906d1cbf319eca1815d342cc4710f
Ianwaswa <ianwaswa2019@gmail.com>
2024-06-18 02:46:27 -0700

    update code

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,21 +1,35 @@
 #!/usr/bin/python3
-""" Script to get the first 10 hot
-    posts on Reddit
+"""script for parsing web data from an api
 """
-from requests import get
+import json
+import requests
+import sys
 
 
 def top_ten(subreddit):
-    """get first 10 hot post for a subreddit"""
-    if subreddit and type(subreddit) is str:
-        url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-        headers = {'user-agent': 'my-app/0.0.1'}
-        params = {'limit': 10}
-        req = get(url, params=params, headers=headers, allow_redirects=False)
-        if req.status_code == 200:
-            data = req.json()
-            posts = data.get('data', {}).get('children', {})
-            for post in posts:
-                print(post.get('data').get('title'))
-        else:
-            print(None)
+    """api call to reddit to get the number of subscribers
+    """
+    base_url = 'https://www.reddit.com/r/'
+    headers = {
+        'User-Agent':
+        'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
+        Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
+    }
+    # grab info about all users
+    url = base_url + '{}/top/.json?count=10'.format(subreddit)
+    response = requests.get(url, headers=headers)
+    resp = json.loads(response.text)
+
+    try:
+        # grab the info about the users' tasks
+        data = resp.get('data')
+        children = data.get('children')
+    except:
+        print('None')
+    if children is None or data is None or len(children) < 1:
+        print('None')
+
+    for i, post_dict in enumerate(children):
+        if i == 10:
+            break
+        print(post_dict.get('data').get('title'))
