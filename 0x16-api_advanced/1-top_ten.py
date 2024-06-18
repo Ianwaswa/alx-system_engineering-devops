30cd8c5333aa207b257f06f3062c84eeef8221c5
Ianwaswa <ianwaswa2019@gmail.com>
2024-06-18 03:05:38 -0700

    update code

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,35 +1,24 @@
 #!/usr/bin/python3
-"""script for parsing web data from an api
-"""
-import json
+'''
+    the module contains the function top_ten
+'''
 import requests
-import sys
+from sys import argv
 
 
 def top_ten(subreddit):
-    """api call to reddit to get the number of subscribers
-    """
-    base_url = 'https://www.reddit.com/r/'
-    headers = {
-        'User-Agent':
-        'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
-        Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
-    }
-    # grab info about all users
-    url = base_url + '{}/top/.json?count=10'.format(subreddit)
-    response = requests.get(url, headers=headers)
-    resp = json.loads(response.text)
-
+    '''
+        returns the top ten posts for a given subreddit
+    '''
+    user = {'User-Agent': 'Lizzie'}
+    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
+                       .format(subreddit), headers=user).json()
     try:
-        # grab the info about the users' tasks
-        data = resp.get('data')
-        children = data.get('children')
-    except:
-        print('None')
-    if children is None or data is None or len(children) < 1:
-        print('None')
+        for post in url.get('data').get('children'):
+            print(post.get('data').get('title'))
+    except Exception:
+        print(None)
 
-    for i, post_dict in enumerate(children):
-        if i == 10:
-            break
-        print(post_dict.get('data').get('title'))
+
+if __name__ == "__main__":
+    top_ten(argv[1])
