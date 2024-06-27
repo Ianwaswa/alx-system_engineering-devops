ba520dfa9970b398c16410f146aa513820d261f2
Ianwaswa <ianwaswa2019@gmail.com>
2024-06-27 03:52:08 -0700

    update code

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,6 @@
+# Modify the OS configuration to allow the holberton user to log in and open files without encountering error messages
+
+exec {'OS security config':
+  command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
+  path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
+}
