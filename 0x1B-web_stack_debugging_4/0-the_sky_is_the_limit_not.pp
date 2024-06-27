d7edd1c0b6ba3fb2c968b7f10e7e50b4b9de8734
Ianwaswa <ianwaswa2019@gmail.com>
2024-06-27 03:48:06 -0700

    update  code

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# Configure Nginx to handle and serve a higher volume of requests
+
+exec {'modify max open files limit setting':
+  command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
+}
